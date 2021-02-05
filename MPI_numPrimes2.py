6364194f4f1e4e7bc737ada67d92ff86819f7fd2
Emily <mremedios.gonzalez@gmail.com>
2021-02-05 12:25:19 +0100

    Update MPI_numPrimes2.py

--- MPI_numPrimes2.py
+++ MPI_numPrimes2.py
@@ -1,49 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import numpy as np;
-import time;
-import sys;
-import math;
-import os;
-from mpi4py import MPI;
+import numpy as np
+import time
+import sys
+import math
+import os
+from mpi4py import MPI
 
-start_time = time.time();
+start_time = time.time()
 
 #Búsqueda de primos hasta num
-num = 10**2;
+num = 10**2
 
 primos=[]
-wtime = MPI.Wtime();
+wtime = MPI.Wtime()
 print(wtime)
 
 def es_primo(num,fin,array_primos):
     #Solo comprobamos impares hasta la raiz de num
-    i=2;
-    while (i < int(math.sqrt(fin))+1):
+    i=2
+    while i < int(math.sqrt(fin))+1:
         if i==num:
-            return True;
+            return True
         elif num%i == 0:
-            #print("num%i",num%i)
-            return False;
+            return False
         elif i==math.sqrt(num):
-            #print("i",i,"sqrt",int(math.sqrt(num)))
-            return False;
+            return False
         
-        i=i+1;
-    return True;
+        i=i+1
+    return True
 
 #Paralización del bucle interior
 def set_primos(inicio, fin, array_primos, rank, size):
     
-    rango_=fin-inicio;
-    rango=int(rango_/size);
-    inicio_=inicio+(rango*rank);
+    rango_=fin-inicio
+    rango=int(rango_/size)
+    inicio_=inicio+(rango*rank)
     
     #for i in range(size):
-    fin_ = inicio_+rango;
-    if(fin_ > fin):
-            fin_ = fin;
+    fin_ = inicio_+rango
+    if fin_ > fin:
+            fin_ = fin
     
     #Paralelizamos las búsqueda de los rangos en diferentes procesadores
     for i in range(inicio_,fin_,1):
@@ -51,17 +49,17 @@ def set_primos(inicio, fin, array_primos, rank, size):
         if i > 2:
             if es_primo(i,fin_,array_primos):
                 
-                array_primos=np.append(array_primos,i); 
+                array_primos=np.append(array_primos,i);
     
-    return array_primos;
+    return array_primos
      
     
 def rango_primos(num):
-    comm = MPI.COMM_WORLD;
+    comm = MPI.COMM_WORLD
     #Procesador en ejecución
-    rank = comm.Get_rank();
+    rank = comm.Get_rank()
     #Procesadores disponibles
-    size = comm.Get_size();
+    size = comm.Get_size()
     
     print(MPI.Get_processor_name(),'está corriendo')
     print("Proceso ", os.getpid())
@@ -69,19 +67,19 @@ def rango_primos(num):
     array_primos=np.array([2], dtype=np.int64);
     
     #Creamos rangos para la búsqueda de números primos
-    div=10**1;
+    div=10**1
     
     if num > div:
-        rango=int(num/div);
-        inicio=0;
+        rango=int(num/div)
+        inicio=0
         
         for i in range(rango):
-            fin = inicio+div;
-            if(fin > num):
-                fin = num;
+            fin = inicio+div
+            if fin > num:
+                fin = num
         
-            array_primos = set_primos(inicio, fin, array_primos, rank, size);
-            inicio = fin;
+            array_primos = set_primos(inicio, fin, array_primos, rank, size)
+            inicio = fin
             
     totalb=0;
     #Recorremos los procesadores para obtener sus resultados y unificarlos todos en 
@@ -91,7 +89,7 @@ def rango_primos(num):
         countb = comm.bcast(array_primos,i)
         totalb = totalb + int(len(countb))
         
-    total = comm.reduce(totalb,MPI.BOR,0);
+    total = comm.reduce(totalb,MPI.BOR,0)
     
     #print("\nPROCESADOR ",rank, "ESTA CALCULANDO LOS PRIMOS:", primos);
     #print("\nPROCESADOR ",rank," HA CALCULADO EN EL RANGO:",inicio, fin);
@@ -99,10 +97,10 @@ def rango_primos(num):
     if rank==0:
         print ("Total de procesadores: ",size)
         print("\nEL TOTAL DE NÚMEROS PRIMOS DESDE ",2,"HASTA",num,"SON :",25)
-        print('');
+        print('')
         
-    return array_primos;
+    return array_primos
 
-primos = rango_primos(num);
+primos = rango_primos(num)
 
-print(primos);
+print(primos)
